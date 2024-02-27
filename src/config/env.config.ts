98d99b571017fd403f78044a434b6d49dde931f3
Gustavo Díaz <genrique@torticity.com>
2024-02-27 19:58:37 -0400

    fix mongodb variable

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 export const envConfig = () => ({
   environment: process.env.NODE_ENV || 'dev',
-  mongo_db: process.env.MONGO_DB,
+  mongodb: process.env.MONGODB,
   port: process.env.PORT || 3000,
   default_limit: process.env.DEFAULT_LIMIT || 10,
 });
